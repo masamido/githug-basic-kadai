64ec09cfbef351143e003652f4bd816c37fbc630
masamido <autumn.tints.midori@gmail.com>
2024-10-25 22:02:17 +0900

    kadai_5
    四則演算をしよう課題提出

---
+++ kadai_05/bmi.js
@@ -0,0 +1,12 @@
+//体重
+let weight =68;
+console.log('体重=' + weight + 'kg');
+
+//身長
+let height =1.7;
+console.log('身長=' + height + 'm')
+
+//BMI
+let bmi = weight/(height*height);
+console.log('BMI=' + bmi);
+
